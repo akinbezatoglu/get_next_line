d91875dc7f3b79474b0eed6a64658ffc41603f99
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-28 00:31:01 +0300

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,10 @@
+*This project has been created as part of the 42 curriculum by abezatog.*
+
+## Description
+The Get Next Line (GNL) project is a C programming task focused on writing a function that returns a single line read from a file descriptor. The function is designed to be called repeatedly in a loop to read a text file one line at a time until the end of the file is reached.
+
+## Algorithm Explanation and Justification
+
+## Instructions
+
+## Resources
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abezatog <abezatog@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/27 23:57:38 by abezatog          #+#    #+#             */
+/*   Updated: 2026/08/28 00:30:35 by abezatog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif

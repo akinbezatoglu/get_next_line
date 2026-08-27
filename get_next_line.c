8ec3cae7e1ab87d40801d001d948e2adf362b446
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-28 02:18:51 +0300

    feat(gnl_v1): the first implementation of gnl without edge cases

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,75 @@
 /*   By: abezatog <abezatog@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/27 23:56:32 by abezatog          #+#    #+#             */
-/*   Updated: 2026/08/28 00:30:37 by abezatog         ###   ########.fr       */
+/*   Updated: 2026/08/28 02:17:42 by abezatog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*ft_read_for_line(int fd, char *line)
+{
+	char		*buffer;
+	ssize_t		r_bytes;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	r_bytes = 1;
+	while (r_bytes > 0)
+	{
+		r_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (r_bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[r_bytes] = '\0';
+		if (!line)
+			line = ft_strdup("");
+		line = ft_strjoin(line, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
-	(void)fd;
-	return ("");
+	static char	*line;
+	char		*res;
+	int			i;
+
+	i = 0;
+	line = ft_read_for_line(fd, line);
+	if (!line || line[0] == '\0')
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] == '\n')
+		i++;
+	res = ft_substr(line, 0, i);
+	line = ft_substr(line, i, ft_strlen(line) - i);
+	return (res);
 }
+
+// #include <stdio.h>
+// #include <fcntl.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("test.txt", O_RDONLY);
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
